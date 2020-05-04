63c61a5e5f77a8da5a12e7c708a1122c0a9719b3
aymanlattar <aymanlattar@aucegypt.edu>
2020-05-04 16:46:39 +0200

    “message”

---
+++ sources/aymanlattar.cpp
@@ -0,0 +1,13 @@
+#include "aymanlattar.h"
+
+void aymanlattar ()
+{
+  cout<< "Name:" << "/n" << "Ayman El-Attar";
+  cout<< "Email:" << "/n" << "aymanlattar@aucegypt.edu";
+}
+
+int main ()
+{
+  aymanlattar();
+  return 0;
+}
